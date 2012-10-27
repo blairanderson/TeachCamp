daf950ff3ed9c9313192b309445cff8799a8bb09
Blair Anderson <blair81@gmail.com>
2012-10-26 18:58:11 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Teachcamp::Application.config.secret_token = '4aa468a57547ed09cbedb5437ab95e3bbdf39b5078751673fbb84ed92df7dca9f5e7261f0024ed827e6298392a96a596f152b3062469379e20408ce3b123e92a'
