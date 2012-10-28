40403a574ab9cc9b301deb64398e3c1ba2ede56c
Blair Anderson <blair81@gmail.com>
2012-10-28 00:12:48 -0700

    adding to gihub

--- config/routes.rb
+++ config/routes.rb
@@ -72,6 +72,7 @@ Teachcamp::Application.routes.draw do
 
   # See how all your routes lay out with "rake routes"
   root :to => 'students#index'
+
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
