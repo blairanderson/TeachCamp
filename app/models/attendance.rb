001d7f767fc628e2e7f6385462da46ef3aa4f94b
Blair Anderson <blair81@gmail.com>
2012-11-18 22:38:08 -0800

    added reports to the student_show for each class

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -3,6 +3,10 @@ class Attendance < ActiveRecord::Base
   belongs_to :classroom
   belongs_to :student
 
+
+  scope :last_week, lambda { where(updated_at: Time.now-1.week..Time.now) }
+  scope :last_month, lambda { where(updated_at: (Time.now-1.month).beginning_of_month..Time.now.beginning_of_month) }
+  scope :month_to_date, lambda { where(updated_at: Time.now.beginning_of_month..Time.now) }
   scope :today, lambda { where(updated_at: (Time.now.to_date).to_time...Time.now) }
 
 end
